ef16626b95e910aa42ecc295a95c0b719c5b0e0d
Rosa <rosarenee@att.net>
2021-05-30 22:35:45 -0500

    commit tree b476bda908af2447e9213fcd77b91fc714330178

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 console.log('%c HI', 'color: firebrick')
+
+function addImage(dogPic){
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4" 
+
+fetch('https://dog.ceo/api/breeds/image/random/4')
+.then(res => res.json())
+.then(dogPic.message.forEach) 
+
+}
